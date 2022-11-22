e6080aa42051fd88b8e8e62fe7ec6660078a8824
nallsaa <89607954+nallsaa@users.noreply.github.com>
2022-11-22 16:10:10 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+objects = []
+status = "";
+
+function preload(){
+  video = createVideo('video.mp4');
+  video.hide();
+}
+
+function setup() {
+  canvas = createCanvas(480, 380);
+  canvas.center();
+  video.hide();
+}
+
+function draw(){
+  image(video, 0, 0, 480, 380);
+  if(status !=""){
+      objectDetector.detect(video, gotResult);
+      for (i=0; i < objects.length; i++){
+          document.getElementById("status").innerHTML = "Status: Objects Detected";
+          document.getElementById("number_of_objects").innerHTML = "Number of objects detected are: "+ objects.length;
+          
+          fill("#FF0000");
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke("#FF0000");
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+      }
+  }
+}
+
+function modelLoaded(){
+
+}
